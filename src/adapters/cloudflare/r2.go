614c8b8df183875bd4640d81dc1026ad836836c4
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-06-24 15:43:11 +0300

    r2: update the adapter

--- src/adapters/cloudflare/r2.go
+++ src/adapters/cloudflare/r2.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"time"
 
 	"api.turistikrota.com/upload/src/domain/cdn"
 	"github.com/sirupsen/logrus"
@@ -37,9 +36,7 @@ func (r *repo) Upload(file []byte, filename string, path ...string) (string, boo
 	}
 	payload := bytes.NewReader(file)
 	req, _err := http.NewRequest("PUT", r.makeUrl(filename, p), payload)
-	req.Header.Add("content-type", "image/png")
-	req.Header.Add("x-amz-content-sha256", "UNSIGNED-PAYLOAD")
-	req.Header.Add("Authorization", r.getAuthKey())
+	req.Header.Add("x-turistikrota-auth", r.config.SecretKey)
 	if _err != nil {
 		logrus.Error(_err)
 		return "", false
@@ -58,16 +55,8 @@ func (r *repo) Upload(file []byte, filename string, path ...string) (string, boo
 	return "", false
 }
 
-func (r *repo) getAuthKey() string {
-	return fmt.Sprintf("AWS4-HMAC-SHA256 Credential=%s/20230622/auto/s3/aws4_request, SignedHeaders=content-length;content-type;host;x-amz-content-sha256;x-amz-date, Signature=71f89d84ada33d0cb01c7ef702997abb6c9b6c940bcb91f01ba0343144041431", r.config.AccessKey)
-}
-
-func (r *repo) getCurrentTime() string {
-	return time.Now().Format("20210901T000000Z")
-}
-
 func (r *repo) makeUrl(filename string, path string) string {
-	return fmt.Sprintf("https://%s.r2.cloudflarestorage.com/%s/%s/%s", r.config.AccountId, r.config.Bucket, path, filename)
+	return fmt.Sprintf("https://upload.worker.turistikrota.com/%s/%s", path, filename)
 }
 
 func (r *repo) makeUploadedUrl(filename string, path string) string {
